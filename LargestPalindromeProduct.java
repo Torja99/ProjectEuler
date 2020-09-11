a88a0318aebb4a8a8371637a9aa2b66784ee39f7
torja99.github.io <istiaqum@mcmaster.ca>
2020-09-11 16:08:25 -0400

    problem4 updated soln to break early

--- LargestPalindromeProduct.java
+++ LargestPalindromeProduct.java
@@ -1,37 +1,40 @@
 public class LargestPalindromeProduct {
     public static void main(String[] args) {
         int factor = 0;
-        String n ="";
+        String n = "";
         int max = 0;
 
-
-        for (int i = 900; i <= 999; i++) {
-            for (int j = 900; j <= 999; j++) {
+        for (int i = 999; i >= 100; i--) {
+            for (int j = 999; j >= 100; j--) {
+                System.out.println("begin");
+                System.out.println(j);
                 factor = i * j;
                 String t = Integer.toString(factor);
                 n = rev(t);
+                // break if factor is less than the max to avoid checking lower values
+                if (factor <= max) {
+                    break;
+
+                }
                 if (t.equals(n)) {
-                  System.out.println("Pot palinddrome: " + factor + "From factors: " + i + ", " + j);
                     if (factor > max) {
                         max = factor;
                     }
-
 
                 }
 
             }
 
         }
-       System.out.println(max);
+        System.out.println(max);
 
     }
 
-    public static String rev (String f) {
+    public static String rev(String f) {
         String n = "";
-        for (int i = f.length()-1; i >= 0; i--) {
+        for (int i = f.length() - 1; i >= 0; i--) {
             n = n + f.charAt(i);
-            
-            
+
         }
         return n;
     }
