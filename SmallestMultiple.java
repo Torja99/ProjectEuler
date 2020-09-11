4d67e4df223d700b226b15a369467df50b094471
torja99.github.io <istiaqum@mcmaster.ca>
2020-09-11 16:28:19 -0400

    problem5 initial solutiom

---
+++ SmallestMultiple.java
@@ -0,0 +1,24 @@
+public class SmallestMultiple {
+    public static void main(String[] args) {
+
+
+        int i = 1;
+        while (div(i) == false){
+            i++;
+        }
+        System.out.println(i);
+    }
+
+    static boolean div(int a) {
+        boolean flag = true;
+        for (int i = 1; i <= 20; i++) {
+            if (!(a % i == 0)) {
+                flag = false;
+
+            }
+
+        }
+        return flag;
+    }
+
+}
