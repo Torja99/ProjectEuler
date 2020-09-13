8a76e8be3d2e930fc1bc10a45d82269c66f10f9d
torja99.github.io <istiaqum@mcmaster.ca>
2020-09-13 18:10:46 -0400

    included summation formula solution

--- SumSquareDiff.java
+++ SumSquareDiff.java
@@ -12,6 +12,13 @@ public class SumSquareDiff {
         sum2 = (int) Math.pow(sum2, 2);
 
         System.out.println(sum2 - sum1);
+        
+        int lim = 100;
+        int sum = 0;
+        int sumSq = 0;
+        sum = lim*(lim +1)/2;
+        sumSq = ((2*lim)+1)*(lim+1)*  lim/6;
+        System.out.println((int)Math.pow(sum,2) - sumSq);
 
     }
 }
