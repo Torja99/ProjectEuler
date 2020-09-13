861eafa3659b29547b25939d6952e99191f1843d
torja99.github.io <istiaqum@mcmaster.ca>
2020-09-13 18:04:13 -0400

    done in one loop

--- SumSquareDiff.java
+++ SumSquareDiff.java
@@ -2,18 +2,13 @@ public class SumSquareDiff {
     public static void main(String[] args) {
         int sum1 = 0;
         int sum2 = 0;
-        
         // sum of square of first 100 hundred
         for (int i = 1; i <= 100; i++) {
             sum1 += Math.pow(i, 2);
-
-        }
-
-        // square sum of first 100
-        for (int i = 1; i <= 100; i++) {
             sum2 += i;
 
         }
+
         sum2 = (int) Math.pow(sum2, 2);
 
         System.out.println(sum2 - sum1);
